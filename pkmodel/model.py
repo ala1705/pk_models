12a0dcd43b29b1fd3ceab8c2b0def1f089162ca4
Matthew Ghosh <matthew.ghosh88@gmail.com>
2023-10-18 17:32:44 +0100

    Model updated with initial implementations for Intravenous and Subcutaneous Models

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -56,13 +56,47 @@ class Model:
 
         :return: A dictionary keyed by the name of the `Compartment` containing each compartment
         """
-        self.compartments["Central"] = Central(self.V_c)
+        compartments = {"Central": Central(self.V_c)}
 
         for i in range(self.num_peripheries):
             volume = self.V_p_list[i]
             transition_rate = self.Q_p_list[i]
-            self.compartments["Periphery " + str(i + 1)] = Periphery(volume, transition_rate)
+            compartments["Periphery " + str(i + 1)] = Periphery(volume, transition_rate)
+
+        return compartments
 
     def add_equations(self) -> dict[str, float]:
         pass
+
+
+class Intravenous(Model):
+    """This describes the Intravenous model implementation for the compartments and equations
+    """
+
+    def __init__(self, clearance_rate: float, dose_rate: float, V_c: float, num_peripheries: int, V_p_list: list[float],
+                 Q_p_list: list[float]):
+        super().__init__(clearance_rate, dose_rate, V_c, num_peripheries, V_p_list, Q_p_list)
+
+    def add_compartments(self) -> dict[str, Compartment]:
+        return super().add_compartments()
+
+
+class Subcutaneous(Model):
+    """This describes the Subcutaneous model implementation for the compartments and equations
+    """
+
+    def __init__(self, clearance_rate: float, dose_rate: float, V_c: float, num_peripheries: int, V_p_list: list[float],
+                 Q_p_list: list[float], V_d, absorption_rate: float):
+        super().__init__(clearance_rate, dose_rate, V_c, num_peripheries, V_p_list, Q_p_list)
+
+        if V_d is float and absorption_rate is float:
+            self.V_d = V_d
+            self.k_a = absorption_rate
+        else:
+            raise TypeError("Dose compartment volume and absorption rate must be floats")
+
+    def add_compartments(self) -> dict[str, Compartment]:
+        compartments = super().add_compartments()
+        compartments["Dose"] = Dose(self.V_d, self.k_a)
+        return compartments
 
