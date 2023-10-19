8ba6f380288bde87e1a5ce897db899434a1c6e24
Thomas Reed <thomas.reed@wolfson.ox.ac.uk>
2023-10-19 09:51:08 +0100

    DEleted q values from classes and find transition function

--- pkmodel/compartment.py
+++ pkmodel/compartment.py
@@ -4,16 +4,14 @@ class Compartment:
     """Class representing a drug compartment
     """
 
-    def __init__(self, volume, drug_quantity):
+    def __init__(self, volume):
         """Initialise compartment class
 
         Args:
             volume (float): volume of compartment [mL]
-            drug_quantity (float): quantity of drug in compartment [ng]
         """
 
         self.volume = volume
-        self.drug_quantity = drug_quantity
 
 
 
@@ -21,15 +19,14 @@ class Central(Compartment):
     """Class representing the central drug compartment
     """
 
-    def __init__(self, volume, drug_quantity):
+    def __init__(self, volume):
         """Initialise central compartment class
 
         Args:
             volume (float): volume of compartment [mL]
-            drug_quantity (float): quantity of drug in compartment [ng]
         """
 
-        super().__init__(volume, drug_quantity)
+        super().__init__(volume)
         
 
 
@@ -37,42 +34,30 @@ class Periphery(Compartment):
     """Class representing a periphery drug compartment
     """
 
-    def __init__(self, volume, drug_quantity, transition_rate):
+    def __init__(self, volume, transition_rate):
         """Initialize periphery compartment class
 
         Args:
             volume (float): volume of compartment [mL]
-            drug_quantity (float): quantity of drug in compartment [ng]
             transition_rate (float): rate of drug transition between central and peripheral compartment [mL/h]
         """
 
-        super().__init__(volume, drug_quantity)
+        super().__init__(volume)
         self.transition_rate = transition_rate
 
-    def find_transition(self, q_c, V_c):
-        """Function to calculate transition expression in diff equation model
 
-        Args:
-            q_c (float): drug quantity in central compartment [ng]
-            V_c (float): volume of central compartment [mL]
-
-        Returns:
-            float: expression for transition rate in diff eq
-        """
-
-        return self.transition_rate * ((q_c / V_c) - (self.drug_quantity / self.volume))
     
 class Dose(Compartment):
     """Class representing the dose compartment
     """
     
-    def __init__(self, volume, drug_quantity, absorption_rate):
+    def __init__(self, volume, absorption_rate):
         """Initialise dose compartment class
 
         Args:
             volume (float): volume of compartment [mL]
-            drug_quantity (float): quantity of drug in compartment [ng]
+            absorption_rate (float): rate of drug absorption [/h]
         """
-        super().__init__(volume, drug_quantity)
+        super().__init__(volume)
         self.absorption_rate = absorption_rate
 
