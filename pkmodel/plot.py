9e7a10182132a4fddbe576bc2d84baf67a05b841
Thomas Reed <thomas.reed@wolfson.ox.ac.uk>
2023-10-19 11:47:40 +0100

    Added plot title

--- pkmodel/plot.py
+++ pkmodel/plot.py
@@ -5,11 +5,16 @@ import numpy as np
 import scipy.integrate
 
 def plot(data):
+    """function to plot model outcome and save plots
 
+    Args:
+        data (dict): dictionary storing all plot data
+    """
 
     t = data["t"]
+    
     q_c = data["Central"]
-    plt.plot(t, q_c, label="q_c")
+    plt.plot(t, q_c, label="Central")
 
     if "Peripheries" in data:
 
@@ -17,17 +22,22 @@ def plot(data):
 
         i = 1
         for p in q_p:
-            plt.plot(t, p, label="q_p"+str(i))
+            plt.plot(t, p, label="Peripheral "+str(i))
             i += 1
 
 
     if "Dose" in data:
 
         q_d = data["Dose"]
-        plt.plot(t, q_d, label="q_d")
+        plt.plot(t, q_d, label="Dose")
 
 
     plt.legend()
     plt.ylabel('drug mass [ng]')
     plt.xlabel('time [h]')
+    plt.title("Drug Mass against Time within Drug Compartments")
+
+    plt.savefig("comparison_plot.png")
     plt.show()
+
+def comparison_plot(data1, data2)
