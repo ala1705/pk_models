ddfa8931279fdc5fcd060859afebc96392996f01
Abbie Evans <abbieevans75@gmail.com>
2023-10-20 10:08:44 +0100

    Updated manual tests

--- pkmodel/main.py
+++ pkmodel/main.py
@@ -3,8 +3,8 @@ from subcutaneous import Subcutaneous
 from plot import plot
 
 # Some manual testing cases - Basic arguments
-intravenous_model_1 = Intravenous(1.0, 1.0, 1.0, 2, [1.0, 2.0], [1.0, 3.0])
-subcutaneous_model_1 = Subcutaneous(1.0, 1.0, 1.0, 2, [1.0, 2.0], [1.0, 3.0], 1.0, 1.0)
+intravenous_model_1 = Intravenous(1.0, 10.0, 1, 1, 1.0, 2, [1.0, 2.0], [1.0, 3.0])
+subcutaneous_model_1 = Subcutaneous(1.0, 1.0, 1, 1, 1.0, 2, [1.0, 2.0], [1.0, 3.0], 1.0, 1.0)
 
 results_intravenous_1 = intravenous_model_1.solve_equations()
 results_subcutaneous_1 = subcutaneous_model_1.solve_equations()
@@ -21,8 +21,8 @@ plot(results_intravenous_2, "all_defaults")
 plot(results_subcutaneous_2, "all_defaults")
 
 # Setting dosage only to zero
-intravenous_model_3 = Intravenous(clearance_rate=1, dose_rate=0, Q_p_list=[2.0])
-subcutaneous_model_3 = Subcutaneous(clearance_rate=1, dose_rate=0, Q_p_list=[2.0], absorption_rate=1.0)
+intravenous_model_3 = Intravenous(clearance_rate=1, X=0, Q_p_list=[2.0])
+subcutaneous_model_3 = Subcutaneous(clearance_rate=1, X=0, Q_p_list=[2.0], absorption_rate=1.0)
 
 results_intravenous_3 = intravenous_model_3.solve_equations()
 results_subcutaneous_3 = subcutaneous_model_3.solve_equations()
@@ -30,8 +30,8 @@ plot(results_intravenous_3, "zero_dosage")
 plot(results_subcutaneous_3, "zero_dosage")
 
 # Setting clearance_rate to zero
-intravenous_model_4 = Intravenous(clearance_rate=0, dose_rate=1, Q_p_list=[2.0])
-subcutaneous_model_4 = Subcutaneous(clearance_rate=0, dose_rate=1, Q_p_list=[2.0], absorption_rate=1.0)
+intravenous_model_4 = Intravenous(clearance_rate=0, X=1, Q_p_list=[2.0])
+subcutaneous_model_4 = Subcutaneous(clearance_rate=0, X=1, Q_p_list=[2.0], absorption_rate=1.0)
 
 results_intravenous_4 = intravenous_model_4.solve_equations()
 results_subcutaneous_4 = subcutaneous_model_4.solve_equations()
